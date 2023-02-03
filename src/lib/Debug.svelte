934c92bcb1c14b4893cb9483068d4fbfa465d7f4
Craig Hopgood <searaig@gmail.com>
2023-02-03 08:23:34 -0500

    install and setup daisyui

--- src/lib/Debug.svelte
+++ src/lib/Debug.svelte
@@ -1,3 +1,3 @@
-<pre class="bg-gray-300 mb-8 p-2">
+<pre class="bg-gray-300 mb-8 p-2 text-black">
 <slot/>
 </pre>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{svelte,js,ts,html}',
-  ],
-  plugins: [
-      require("@tailwindcss/typography")
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{svelte,js,ts,html}',
+    ],
+    plugins: [
+        require( "@tailwindcss/typography" ),
+        require( "daisyui" )
+    ],
+    daisyui: {
+        styled: true,
+        themes: true,
+        base: true,
+        utils: true,
+        logs: true,
+        rtl: false,
+        prefix: "",
+        darkTheme: "dark",
+    },
+    theme: {
+        extend: {},
+    },
 }
