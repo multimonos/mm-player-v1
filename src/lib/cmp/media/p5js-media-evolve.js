d24adc80509b52c25cfd7db9e4b84236b01efa4f
Craig Hopgood <searaig@gmail.com>
2023-02-10 18:14:49 -0500

    add convenient way to dispose of an audio context in p5js by convention

--- src/lib/cmp/media/p5js-media-evolve.js
+++ src/lib/cmp/media/p5js-media-evolve.js
@@ -15,16 +15,18 @@ const evoleIfNotExists = ( prop, fn ) => p5i => {
 const pause = p5i => () => {
     if ( ! p5i ) return
     p5i.noLoop()
-}
-
-const resume = p5i => () => {
-    if ( ! p5i ) return
-    p5i.loop()
+    p5i?.audioContext?.suspend?.() // This is a convention only.
 }
 
 const play = p5i => () => {
     if ( ! p5i ) return
     p5i.loop()
+    p5i?.audioContext?.resume?.() // This is a convention only.
+}
+
+const destroy = p5i => async () => {
+console.log('destroy was called on', p5i)
+    await p5i?.audioContext?.close?.() // This is a convention only.
 }
 
 const screenshot = p5i => track => {
@@ -44,18 +46,20 @@ const screenshot = p5i => track => {
 }
 
 /**
- * Evolve p5js instance with media player interface methods
- * that we may need for playback and other controls.
+ * Evolve p5js instance with our media player interface methods,
+ * so, that we may need for playback and other controls.
  *
- * Note: We only add methods that do not already exist.
- *
+ * NOTES
+ * - we only add methods that do not already exist.
+ * - the `audioContext` can be attached to the p5js instance, but, doesn't have to be ( convention only )
+ * - if the `audioContext` is not attached, by same name, to the p5js instance, then developer should write the own custom pause, play, resume methods
  * @param a p5js Instance ( already mounted to dom with new p5( sketch, canvasNode )
  * @returns p5jsInstance
  */
 export const p5jsMediaEvolve = p5i =>
     pipe(
         evoleIfNotExists( 'pause', pause ),
-        evoleIfNotExists( 'resume', resume ),
         evoleIfNotExists( 'play', play ),
+        evoleIfNotExists( 'destroy', destroy ),
         evoleIfNotExists( 'screenshot', screenshot ),
     )( p5i )
