8d70f22485b9b87a52d92e5623d90fb1391ec7bc
Craig Hopgood <searaig@gmail.com>
2023-02-10 12:28:10 -0500

    add ScreenshotEvent

--- src/lib/state-machine/events.js
+++ src/lib/state-machine/events.js
@@ -2,19 +2,20 @@
 ////////////////////
 
 // player events
-export const PlayEvent = 'play'
-export const PauseEvent = 'pause'
-export const ResumeEvent = 'resume'
-export const ProgressEvent = 'progress'
+export const PlayEvent = 'player:play'
+export const PauseEvent = 'player:pause'
+export const ResumeEvent = 'player:resume'
+export const ProgressEvent = 'player:progress'
 export const EvolveMediaEvent = 'media:evolve'
+export const ScreenshotEvent = 'media:screenshot'
 
 // queue events
-export const QueueNextEvent = 'next'
-export const QueuePreviousEvent = 'previous'
+export const QueueNextEvent = 'queue:next'
+export const QueuePreviousEvent = 'queue:previous'
 export const QueueReplaceEvent = 'queue.replace'
 export const QueueAppendEvent = 'queue.append'
 export const QueueClearEvent = 'queue.clear'
 
 // other
-export const FullscreenToggleEvent = 'fullscreen.toggle'
+export const FullscreenToggleEvent = 'fullscreen:toggle'
 export const ErrorEvent = 'error'
