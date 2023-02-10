dec137472a8a80391fe8212ddda30cbb14530649
Craig Hopgood <searaig@gmail.com>
2023-02-10 12:16:47 -0500

    stringify bugfix

--- src/lib/string-utils.js
+++ src/lib/string-utils.js
@@ -1,7 +1,14 @@
-    const fyReplacer = ( key, value ) => {
-        if ( typeof value === 'function' ) return value.name
+    import {stringify}  from "flatted"
+const fyReplacer = ( key, value ) => {
+    try {
+
+   console.log(typeof value,{value})
+        if ( typeof value === 'function' ) return value.name || 'function'
         if ( value === null ) return "null"
         return value
+    } catch ( e ) {
+        return e
+    }
 
     }
-    export const fy = o => JSON.stringify( o, fyReplacer, 2 )
+    export const fy = o => stringify( o, null, 2)
