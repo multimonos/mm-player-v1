c44dc79a964a212cfd422e415f9032bc55eaaf5b
Craig Hopgood <searaig@gmail.com>
2023-02-14 10:51:24 -0500

    better json stringify

--- src/lib/string-utils.js
+++ src/lib/string-utils.js
@@ -1,4 +1,3 @@
-    import {stringify}  from "flatted"
 // const fyReplacer = ( key, value ) => {
 //     try {
 //
@@ -11,4 +10,35 @@
 //     }
 //
 //     }
-    export const fy = o => stringify( o, null, 2)
+
+const getCircularReplacer = () => {
+    const seen = new WeakSet()
+
+    return ( key, value ) => {
+        if ( typeof value === "object" && value !== null ) {
+            if ( seen.has( value ) ) {
+                return
+            }
+            seen.add( value )
+        }
+        return value
+    }
+}
+
+export function fy( obj ) {
+    let cache = []
+    let str = JSON.stringify( obj, function ( key, value ) {
+        if ( value === null ) return "null"
+        if ( typeof value === "object" && value !== null ) {
+            if ( cache.indexOf( value ) !== -1 ) {
+                // Circular reference found, discard key
+                return
+            }
+            // Store value in our collection
+            cache.push( value )
+        }
+        return value
+    }, 2 )
+    cache = null // reset the cache
+    return str
+}
