d7b03b13f135dda779c6e42d0bbafbd45956db4d
Craig Hopgood <searaig@gmail.com>
2023-02-16 15:11:21 -0500

    tell vite to ignore variable in dynamic import /* @vite-ignore */

--- src/routes/dynamic-import-tests/+page.svelte
+++ src/routes/dynamic-import-tests/+page.svelte
@@ -6,26 +6,26 @@
 
     const remote = async (baseuri) => {
         // dynamic remote import with single export
-        const { one } = await import(`${baseuri}/test/one.bundle.js`)
+        const { one } = await import(/* @vite-ignore */`${baseuri}/test/one.bundle.js`)
         one()
 
         // dynamic remote import with multiple exports
-        const { oneOfTwo, twoOfTwo } = await import(`${baseuri}/test/one-and-two.bundle.js`)
+        const { oneOfTwo, twoOfTwo } = await import(/* @vite-ignore */`${baseuri}/test/one-and-two.bundle.js`)
         oneOfTwo()
         twoOfTwo()
 
         // dynamic remote import a p5js sketch defined as an instance named "sketch"
         const p5 = await import('p5')
-        const {  sketch } = await import(`${baseuri}/test/p5js-sketch.bundle.js`)
+        const {  sketch } = await import(/* @vite-ignore */`${baseuri}/test/p5js-sketch.bundle.js`)
         console.log('PASS', { sketch } )
         new p5.default( sketch, 'stuff' )
 
         // dynamic remote import with querystring - called script parses the querystring
-        const { importWithQuerystring } = await import(`${baseuri}/test/querystring.bundle.js?foo=bar&bam=yarr`)
+        const { importWithQuerystring } = await import(/* @vite-ignore */`${baseuri}/test/querystring.bundle.js?foo=bar&bam=yarr`)
         importWithQuerystring()
 
         // dynamic remote import a file, unbundled that depends on custom code only
-        const { importImportsTestUtil } = await import(`${baseuri}/test/import-util.bundle.js`)
+        const { importImportsTestUtil } = await import(/* @vite-ignore */`${baseuri}/test/import-util.bundle.js`)
         importImportsTestUtil()
     }
 
