33c131dad20ce54a9099ce70e35ff3b6ddfb7c79
Craig Hopgood <searaig@gmail.com>
2023-02-16 17:32:53 -0500

    basic media tests at /media-tests

--- src/routes/media-tests/+page.svelte
+++ src/routes/media-tests/+page.svelte
@@ -4,35 +4,35 @@
     import { onMount } from "svelte"
 
 
-    const remote = async (baseuri) => {
+    const remote = async ( baseuri ) => {
         // dynamic remote import with single export
-        const { one } = await import(/* @vite-ignore */`${baseuri}/test/one.bundle.js`)
+        const { one } = await import(/* @vite-ignore */`${ baseuri }/test/one.bundle.js`)
         one()
 
         // dynamic remote import with multiple exports
-        const { oneOfTwo, twoOfTwo } = await import(/* @vite-ignore */`${baseuri}/test/one-and-two.bundle.js`)
+        const { oneOfTwo, twoOfTwo } = await import(/* @vite-ignore */`${ baseuri }/test/one-and-two.bundle.js`)
         oneOfTwo()
         twoOfTwo()
 
         // dynamic remote import a p5js sketch defined as an instance named "sketch"
         const p5 = await import('p5')
-        const {  sketch } = await import(/* @vite-ignore */`${baseuri}/test/p5js-sketch.bundle.js`)
-        console.log('PASS', { sketch } )
+        const { sketch } = await import(/* @vite-ignore */`${ baseuri }/test/p5js-sketch.bundle.js`)
+        console.log( 'PASS', { sketch } )
         new p5.default( sketch, 'stuff' )
 
         // dynamic remote import with querystring - called script parses the querystring
-        const { importWithQuerystring } = await import(/* @vite-ignore */`${baseuri}/test/querystring.bundle.js?foo=bar&bam=yarr`)
+        const { importWithQuerystring } = await import(/* @vite-ignore */`${ baseuri }/test/querystring.bundle.js?foo=bar&bam=yarr`)
         importWithQuerystring()
 
         // dynamic remote import a file, unbundled that depends on custom code only
-        const { importImportsTestUtil } = await import(/* @vite-ignore */`${baseuri}/test/import-util.bundle.js`)
+        const { importImportsTestUtil } = await import(/* @vite-ignore */`${ baseuri }/test/import-util.bundle.js`)
         importImportsTestUtil()
     }
 
     onMount( async () => {
 
-        remote('https://mm-media.netlify.app')
-        remote('http://mm-media.test')
+        remote( 'https://mm-media.netlify.app' )
+        remote( 'http://mm-media.test' )
 
     } )
 </script>
