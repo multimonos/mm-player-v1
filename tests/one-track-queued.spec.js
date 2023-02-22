720ae68ab859083e65444425c75c424095a81ef7
Craig Hopgood <searaig@gmail.com>
2023-02-22 11:51:40 -0500

    add 1 track queued test

---
+++ tests/one-track-queued.spec.js
@@ -0,0 +1,47 @@
+import { expect, test } from "@playwright/test"
+import { History, NowPlaying, Queue, Tracks, Transport } from "./PageObjects.js"
+
+
+test.describe( `Queue one track`, () => {
+
+    test.beforeEach( async ( { page } ) => {
+        await page.goto( "/testing" )
+        const track = await page.click( Tracks.image1 )
+    } )
+
+    test.describe( `Queue`, () => {
+        test( `has length 1`, async ( { page } ) => {
+            await expect( page.locator( Queue.items ) ).toHaveCount( 1 )
+        } )
+    } )
+
+    test.describe( `History`, () => {
+        test( `is empty`, async ( { page } ) => {
+            await expect( page.locator( History.empty ) ).toBeVisible()
+        } )
+    } )
+
+    test.describe( `Now Playing`, () => {
+        test( `is empty`, async ( { page } ) => {
+            await expect( page.locator( NowPlaying.empty ) ).toBeVisible()
+        } )
+    } )
+
+    test.describe( `Transport`, () => {
+        test( `previous disabled`, async ( { page } ) => {
+            await expect( page.locator( Transport.previous ) ).toBeDisabled()
+        } )
+        test( `play enabled`, async ( { page } ) => {
+            await expect( page.locator( Transport.play ) ).toBeEnabled()
+        } )
+        test( `next enabled`, async ( { page } ) => {
+            await expect( page.locator( Transport.next ) ).toBeEnabled()
+        } )
+        test( `pause not visible`, async ( { page } ) => {
+            await expect( page.locator( Transport.pause ) ).not.toBeVisible()
+        } )
+        test( `loading not visible`, async ( { page } ) => {
+            await expect( page.locator( Transport.loading ) ).not.toBeVisible()
+        } )
+    } )
+} )
