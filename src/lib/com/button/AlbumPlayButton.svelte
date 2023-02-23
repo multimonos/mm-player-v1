f41370da5ebbc878e4df0f0b410c1832ee16525e
Craig Hopgood <searaig@gmail.com>
2023-02-23 18:45:39 -0500

    add AlbumPlayButton.svelte

---
+++ src/lib/com/button/AlbumPlayButton.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Icon from "$lib/com/Icon.svelte"
+</script>
+
+
+<button data-tid="play-btn" type="button" class="btn btn-circle btn-lg hover:text-accent" on:click>
+    <Icon icon="mdi:play"  size="lg" />
+</button>
--- src/routes/albums/[slug]/+page.svelte
+++ src/routes/albums/[slug]/+page.svelte
@@ -1,6 +1,25 @@
 <script>
-import {fy} from "$lib/util/string.js"
+import { route } from "$lib/config/routes.js"
+import { goto } from "$app/navigation.js"
+import { service } from "$lib/state-machine/app-machine.js"
+import { QueueReplaceEvent } from "$lib/state-machine/events.js"
+import { fy } from "$lib/util/string.js"
+
+// com
+import AlbumPlayButton from "$lib/com/button/AlbumPlayButton.svelte"
+
+// props
 export let data
+
+// fns
+const playAlbum = album => e => {
+    goto( route( "@debug" ) )
+
+    service.send( { type: QueueReplaceEvent, tracks: [ ...album.tracks ] } )
+}
+// reactives
+$: album = data.album
 </script>
 
-<pre>{fy(data.album)}</pre>
+<AlbumPlayButton on:click={playAlbum(album)}/>
+<pre>{fy( data.album )}</pre>
