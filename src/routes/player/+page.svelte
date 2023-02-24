b25f07d42614221bc8a1b6786fe1f04f6228211e
Craig Hopgood <searaig@gmail.com>
2023-02-24 16:50:29 -0500

    enable view of media on @player

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-// import { RenderableTag } from "$lib/state-machine/tags.js"
-// import { service } from "$lib/state-machine/app-machine.js"
+import { RenderableTag } from "$lib/state-machine/tags.js"
+import { service } from "$lib/state-machine/app-machine.js"
 
 // com
-// import Media from "$lib/com/media/Media.svelte"
+import Media from "$lib/com/media/Media.svelte"
 </script>
-<!--{#if ($service.hasTag( RenderableTag )) && $service.context.media?.component}-->
-<!--    <div class="h-full flex justify-center items-center overflow-x-hidden overflow-y-hidden">-->
-<!--        <Media component={$service.context.media.component} props={$service.context.media.componentProps}/>-->
-<!--    </div>-->
-<!--{/if}-->
+{#if ($service.hasTag( RenderableTag )) && $service.context.media?.component}
+    <div class="h-full flex justify-center items-center overflow-x-hidden overflow-y-hidden">
+        <Media component={$service.context.media.component} props={$service.context.media.componentProps}/>
+    </div>
+{/if}
