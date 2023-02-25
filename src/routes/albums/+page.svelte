60cb51c60473d320fbcbbac3200d9f40a86eb6ec
Craig Hopgood <searaig@gmail.com>
2023-02-25 08:57:19 -0500

    sketch out albums/*

--- src/routes/albums/+page.svelte
+++ src/routes/albums/+page.svelte
@@ -1,10 +1,29 @@
 <script>
 import { route } from "$lib/config/routes.js"
 
+// com
+import ArtistLinks from "$lib/com/artist/ArtistLinks.svelte"
+import AlbumType from "$lib/com/album/AlbumType.svelte"
+import IfDebug from "$lib/com/util/IfDebug.svelte"
 
+// props
 export let data
 </script>
 
-{#each data.albums as album}
-    <p><a href={route("@album", album)}>{album.name}</a></p>
-{/each}
+<div class="flex flex-col space-y-2">
+
+    {#each data.albums as album}
+        <a class="flex items-center space-x-4" href={route('@album', album)}>
+            <div class="flex-none">
+               <img class="object-cover w-16 h-16" alt="{album.name}" src={album.images[0]?.url || '/1.png'}/>
+            </div>
+            <div class="flex-1">
+                <p class="text-md">{album.name}</p>
+                <p class="text-sm text-gray-400"><AlbumType type={album.album_type}/> &bull; <ArtistLinks artists={album.artists}/></p>
+            </div>
+        </a>
+    {/each}
+</div>
+
+
+<IfDebug value={data.albums}/>
--- src/routes/albums/[slug]/+page.svelte
+++ src/routes/albums/[slug]/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 import { service } from "$lib/state-machine/app-machine.js"
 import { QueueReplaceEvent } from "$lib/state-machine/events.js"
-import { fy } from "$lib/util/string.js"
 
 // com
+import IfDebug from "$lib/com/util/IfDebug.svelte"
 import AlbumPlayButton from "$lib/com/button/AlbumPlayButton.svelte"
+import AlbumDuration from "$lib/com/album/AlbumDuration.svelte"
+import MoreButton from "$lib/com/button/MoreButton.svelte"
 
 // props
 export let data
@@ -13,9 +15,50 @@ export let data
 const playAlbum = album => e =>
     service.send( { type: QueueReplaceEvent, tracks: [ ...album.tracks ] } )
 
+const playTrack = track => e =>
+    service.send( { type: QueueReplaceEvent, tracks: [ { ...track } ] } )
+
 // reactives
 $: album = data.album
 </script>
 
-<AlbumPlayButton on:click={playAlbum(album)}/>
-<pre>{fy( data.album )}</pre>
+
+<div id="album-hero" class="flex flex-col">
+    <div class="mx-auto mb-4">
+        <img class="object-cover w-36 h-36" src={album.images[0]?.url || '/1.png'}/>
+    </div>
+    <div class="flex flex-col">
+        <h1 class="text-2xl font-bold mb-2">{album.name}</h1>
+        <p class="text-xs">
+            {album.tracks.length} tracks
+            &bull;
+            <AlbumDuration {album}/>
+        </p>
+    </div>
+</div>
+
+
+<div class="flex justify-end">
+    <AlbumPlayButton on:click={playAlbum(album)}/>
+</div>
+
+
+{#if album.tracks}
+    <div id="album-tracks" class="flex flex-col">
+        {#each album.tracks as track, n }
+            <div class="flex items-center space-x-2">
+                <div class="flex-none p-2">
+                    <button class="btn btn-md btn-ghost text-xs" on:click={playTrack(track)}>{n + 1}</button>
+                </div>
+                <div class="flex-1">
+                    <span>{track.name}</span>
+                </div>
+                <div class="flex-none">
+                    <MoreButton/>
+                </div>
+            </div>
+        {/each}
+    </div>
+{/if}
+
+<IfDebug value={data.album}/>
