d84d6d71932645ee62461c0e3bf1b281a535901a
Craig Hopgood <searaig@gmail.com>
2023-02-25 08:56:38 -0500

    update test case names

--- src/lib/data/albums/test-cases.js
+++ src/lib/data/albums/test-cases.js
@@ -6,9 +6,9 @@ export const album = {
     images: [],
     artists: [ 'multimonos' ],
     tracks: [
-        { id: 'image1', name: '🧪 image 1', duration: 2000, media: { media_type: 'image', url: `/1.png` } },
-        { id: 'image2', name: '🧪 image 2', duration: 2000, media: { media_type: 'image', url: `/2.png` } },
-        { id: 'image3', name: '🧪 image 3', duration: 2000, media: { media_type: 'image', url: `/3.png` } },
+        { id: 'image1', name: 'Image #1 Test', duration: 2000, media: { media_type: 'image', url: `/1.png` } },
+        { id: 'image2', name: 'Image #2 Test', duration: 2000, media: { media_type: 'image', url: `/2.png` } },
+        { id: 'image3', name: 'Image #3 Test', duration: 2000, media: { media_type: 'image', url: `/3.png` } },
         // { id: 'image5min', name: '🧪 image 5 mins', duration: 5*60*1000, media: { media_type: 'image', url: `/3.png` } },
         { media: { media_type: 'p5js', url: `PUBLIC_MEDIA_URL/test/p5js/red.bundle.js` } },
         { media: { media_type: 'p5js', url: `PUBLIC_MEDIA_URL/test/p5js/blue.bundle.js` } },
