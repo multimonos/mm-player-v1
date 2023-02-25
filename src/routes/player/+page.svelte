eb3ed3fbb561cb54a11b217ee3aaf35c0b9bc442
Craig Hopgood <searaig@gmail.com>
2023-02-25 12:19:50 -0500

    add a fade in transition for player

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 import { onDestroy, onMount } from "svelte"
+import { fade } from "svelte/transition"
 import { RenderableTag } from "$lib/state-machine/tags.js"
 import { CancelEvent } from "$lib/state-machine/events.js"
 import { service } from "$lib/state-machine/app-machine.js"
 
 // com
 import Media from "$lib/com/media/Media.svelte"
+
 
 onMount( () => {
     console.log( '@player mounted' )
@@ -17,7 +19,7 @@ onDestroy( () => {
 } )
 </script>
 {#if ($service.hasTag( RenderableTag )) && $service.context.media?.component}
-    <div class="h-full flex flex-col justify-center items-center overflow-x-hidden overflow-y-clip bg-info/30">
+    <div class="h-full flex flex-col justify-center items-center overflow-x-hidden overflow-y-clip" in:fade>
         <Media component={$service.context.media.component} props={$service.context.media.componentProps}/>
     </div>
 {/if}
