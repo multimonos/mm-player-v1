0ce40affbed1c1b3ec7a97dd4b75617a220f4bf6
Craig Hopgood <searaig@gmail.com>
2023-02-25 09:55:12 -0500

    make links optional in ArtistLinks.svelte

--- src/lib/com/artist/ArtistLinks.svelte
+++ src/lib/com/artist/ArtistLinks.svelte
@@ -1,12 +1,15 @@
 <script>
-    import { route } from "$lib/config/routes.js"
-    import { commaIf } from "$lib/util/string.js"
+import { route } from "$lib/config/routes.js"
+import { commaIf } from "$lib/util/string.js"
 
-    // props
-    export let artists = []
+// props
+export let artists = []
+export let link = true
 </script>
 {#if artists.length}
     {#each artists as artist, i}
-        <a href="{route('@artist', artist)}" class="hover:underline">{artist.id}</a>{commaIf( i < artists.length - 1 )}
+        {#if link}<a href="{route('@artist', artist)}" class="hover:underline">{artist.id}</a>{commaIf( i < artists.length - 1 )}
+        {:else}<span>{artist.id}</span>{commaIf( i < artists.length - 1 )}
+        {/if}
     {/each}
 {/if}
