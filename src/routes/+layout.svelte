3b88bbe2a6af757c7520f84ae5b82ffc1126c9c4
Craig Hopgood <searaig@gmail.com>
2023-02-27 10:26:11 -0500

    aggresively create the audioContext

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@ import "../app.css"
 import { afterNavigate } from "$app/navigation.js"
 import { drawerOpen } from "$lib/stores.js"
 import { service } from "$lib/state-machine/app-machine.js"
+import { AudioCreateEvent } from "$lib/state-machine/events.js"
 // com
 import Footer from "$lib/layout/Footer.svelte"
 import Toasts from "$lib/com/Toasts.svelte"
@@ -13,7 +14,12 @@ import Navbar from "$lib/layout/Navbar.svelte"
 import ServiceTest from "$lib/com/util/ServiceTest.svelte"
 import PrimaryNavigation from "$lib/layout/PrimaryNavigation.svelte"
 import Queue from "$lib/com/Queue.svelte"
+import { onMount } from "svelte"
 
+
+onMount( () => {
+    service.send( AudioCreateEvent )
+} )
 
 afterNavigate( () => {
     setTimeout( () => { // scroll fix
@@ -22,35 +28,35 @@ afterNavigate( () => {
 } )
 </script>
 
-    <div class="drawer">
+<div class="drawer">
 
-        <input id="my-drawer" type="checkbox" class="drawer-toggle" bind:checked={$drawerOpen}/>
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" bind:checked={$drawerOpen}/>
 
-        <div class="drawer-content flex flex-col">
+    <div class="drawer-content flex flex-col">
 
-            <Navbar/>
+        <Navbar/>
 
-            <main id="main" class="overflow-y-auto" style="height: calc(100% - 8rem)"><!-- used to be h-full -->
-                <slot/>
-            </main>
+        <main id="main" class="overflow-y-auto" style="height: calc(100% - 8rem)"><!-- used to be h-full -->
+            <slot/>
+        </main>
 
-            <Footer/>
-
-        </div>
-
-        <div class="drawer-side">
-            <label for="my-drawer" class="drawer-overlay"></label>
-            <aside class="bg-base-200 w-80 overflow-y-scroll">
-                <div class="h-4"></div>
-                <PrimaryNavigation on:click={()=>$drawerOpen=false}/>
-                <Queue/>
-                <ul class="menu menu-compact flex flex-col p-0 px-4"></ul>
-                <div class="from-base-200 pointer-events-none sticky bottom-0 flex h-20 bg-gradient-to-t to-transparent"></div>
-            </aside>
-        </div>
+        <Footer/>
 
     </div>
 
-    <Toasts toasts={$service.context.toasts}/>
+    <div class="drawer-side">
+        <label for="my-drawer" class="drawer-overlay"></label>
+        <aside class="bg-base-200 w-80 overflow-y-scroll">
+            <div class="h-4"></div>
+            <PrimaryNavigation on:click={()=>$drawerOpen=false}/>
+            <Queue/>
+            <ul class="menu menu-compact flex flex-col p-0 px-4"></ul>
+            <div class="from-base-200 pointer-events-none sticky bottom-0 flex h-20 bg-gradient-to-t to-transparent"></div>
+        </aside>
+    </div>
+
+</div>
+
+<Toasts toasts={$service.context.toasts}/>
 
 <ServiceTest svc={$service}/>
