1c145790aa741ee7ac29b7c62eaca9fe9a2f20da
Craig Hopgood <searaig@gmail.com>
2023-02-28 18:56:30 -0500

    add + reorg events

--- src/lib/state-machine/events.js
+++ src/lib/state-machine/events.js
@@ -5,19 +5,21 @@
 export const PlayEvent = 'player:play'
 export const PauseEvent = 'player:pause'
 export const ResumeEvent = 'player:resume'
-export const ProgressEvent = 'player:progress'
 export const CancelEvent = 'player:cancel'
+export const SkippingEvent = 'player:skipping'
+
+// media
 export const EvolveMediaEvent = 'media:evolve'
 export const ScreenshotEvent = 'media:screenshot'
 
 // audio
-export const AudioCreateEvent= 'audio:create'
-export const AudioResumeEvent= 'audio:resume'
-export const AudioPauseEvent= 'audio:pause'
+export const AudioResumeEvent = 'audio:resume'
+export const AudioPauseEvent = 'audio:pause'
 
 // timer events
 export const TimerStartEvent = 'timer:start'
-export const TimerStopEvent= 'timer:stop'
+export const TimerStopEvent = 'timer:stop'
+export const TimerProgressEvent = 'timer:progress'
 
 // queue events
 export const QueueNextEvent = 'queue:next'
