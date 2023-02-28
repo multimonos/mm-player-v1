3afc1f9d267218e0cf606f84c5594351439127fc
Craig Hopgood <searaig@gmail.com>
2023-02-28 18:57:50 -0500

    update prepare-asyn-service to delegate load of audio assets to the sketch

--- src/lib/state-machine/service/media-prepare-async-service.js
+++ src/lib/state-machine/service/media-prepare-async-service.js
@@ -1,5 +1,26 @@
-export const mediaPrepareAsyncService = ( context ) => {
-    context.debug && console.log( context.media.params ?? 'no-params-object' )
-    const params = context.media.params ?? {}
-    return context.media.ref.prepare( { params } )
-}
+/*
+ import { AudioBufferSourceNode } from "standardized-audio-context"
+
+
+ const createAudioSources = async ( audioContext, urls ) => { // not in use right now
+ const responses = await Promise.all( urls.map( url => fetch( url ) ) )
+ const arrayBuffers = await Promise.all( responses.map( res => res.arrayBuffer() ) )
+ const buffers = await Promise.all( arrayBuffers.map( buf => audioContext.decodeAudioData( buf ) ) )
+ const sources = buffers.map( buf => {
+ const source = new AudioBufferSourceNode( audioContext ) //.createBufferSource()
+ source.buffer = buf
+ return source
+ } )
+ return sources
+ }
+ */
+
+
+export const mediaPrepareAsyncService = async ( context ) =>
+    new Promise( async ( resolve, reject ) => {
+        context.debug && console.log( context.media.params ?? 'no-params-object' )
+        const params = context.media.params ?? {}
+        await context.media.ref.prepare( { params, audioContext: context.audioContext } )
+
+        return resolve( true )
+    } )
