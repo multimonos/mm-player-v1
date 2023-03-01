2f55d1b6ad48ac9ed532f51e5460978435ac4cdc
Craig Hopgood <searaig@gmail.com>
2023-02-28 19:03:06 -0500

    update test-audio album

--- src/lib/data/albums/test-audio.test.js
+++ src/lib/data/albums/test-audio.test.js
@@ -9,32 +9,52 @@ export const album = {
     artists: [ 'multimonos' ],
     tracks: [
         {
-            name: "Frog sample",
+            name: "Western diamonback [1]",
+            media: {
+                media_type: 'p5js',
+                url: `/src/lib/data/sketch/audio.test.js`,
+                params: {
+                    audioUrl: "https://res.cloudinary.com/multimonos/video/upload/v1618331761/audio/animals/bbc_western-di_nhu0510004.mp3"
+                }
+            }
+        },
+        // { slug: 'image1', name: 'Image #1 Test', duration: 2000, media: { media_type: 'image', url: `/1.png` } },
+        // {
+        //     name: "Toad sample",
+        //     media: {
+        //         media_type: 'p5js',
+        //         url: `/src/lib/data/sketch/audio.test.js`,
+        //         params: {
+        //             audioUrl: 'https://res.cloudinary.com/multimonos/video/upload/v1618331073/audio/animals/bbc_giant-toad_nhu0501904.mp3'
+        //         }
+        //     }
+        // },
+        // { slug: 'image2', name: 'Image #2 Test', duration: 2000, media: { media_type: 'image', url: `/2.png` } },
+        {
+            name: "Wolf sample [2]",
+            media: {
+                media_type: 'p5js',
+                url: `/src/lib/data/sketch/audio.test.js`,
+                params: {
+                    audioUrl: "https://res.cloudinary.com/multimonos/video/upload/v1612041823/audio/animals/wolf.mp3"
+                }
+            }
+        },
+        {
+            name: "Frog sample [3]",
             media: {
                 media_type: 'p5js',
                 url: `/src/lib/data/sketch/audio.test.js`,
                 params: { audioUrl: 'https://res.cloudinary.com/multimonos/video/upload/v1618331080/audio/animals/bbc_snoring-pu_nhu0501912.mp3' }
             }
         },
-        { slug: 'image1', name: 'Image #1 Test', duration: 2000, media: { media_type: 'image', url: `/1.png` } },
         {
-            name: "Toad sample",
+            name: "Them miner beeezz [4]",
             media: {
                 media_type: 'p5js',
                 url: `/src/lib/data/sketch/audio.test.js`,
                 params: {
-                    audioUrl: 'https://res.cloudinary.com/multimonos/video/upload/v1618331073/audio/animals/bbc_giant-toad_nhu0501904.mp3'
-                }
-            }
-        },
-        { slug: 'image2', name: 'Image #2 Test', duration: 2000, media: { media_type: 'image', url: `/2.png` } },
-        {
-            name: "Gibbon sample",
-            media: {
-                media_type: 'p5js',
-                url: `/src/lib/data/sketch/audio.test.js`,
-                params: {
-                    audioUrl: "https://res.cloudinary.com/multimonos/video/upload/v1618331080/audio/animals/bbc_agile-gibb_nhu0501811.mp3",
+                    audioUrl: "https://res.cloudinary.com/multimonos/video/upload/v1618331759/audio/animals/bbc_solitary-m_nhu0504315.mp3"
                 }
             }
         },
