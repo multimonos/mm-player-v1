03440d2b9ecd6a268d22d57cd95ea1cf377ccd7f
Craig Hopgood <searaig@gmail.com>
2023-03-06 22:14:27 -0500

    error check in NowPlaying.svelte

--- src/lib/com/NowPlaying.svelte
+++ src/lib/com/NowPlaying.svelte
@@ -8,7 +8,7 @@ export let track = null
     {#if track}
         <div data-tid="now-playing-item" class="flex items-center">
             <div class="flex-none hidden sm:block block mr-2 rounded">
-                <img class="object-cover w-10 h-10" src={track.album.images[0]?.url || '/1.png'}/>
+                <img class="object-cover w-10 h-10" src={track.album?.images[0]?.url || '/1.png'}/>
             </div>
             <div>
                 <p data-tid="current-track-name" class="text-sm w-40 h-5 overflow-hidden whitespace-nowrap text-ellipsis">{track.name}</p>
