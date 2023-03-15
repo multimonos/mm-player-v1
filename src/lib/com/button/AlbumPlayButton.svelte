8f3a3e4d8a6132081140252842920077b12649ba
Craig Hopgood <searaig@gmail.com>
2023-03-15 16:25:38 -0400

    create PlayAlbumButton.svelte for AlbumCard

--- src/lib/com/button/AlbumPlayButton.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Icon from "$lib/com/icon/Icon.svelte"
-</script>
-
-
-<button data-tid="play-album-btn" type="button" class="btn btn-circle btn-lg hover:text-accent" on:click>
-    <Icon icon="mdi:play"  size="lg" />
-</button>
---
+++ src/lib/com/button/PlayAlbumButton.svelte
@@ -0,0 +1,8 @@
+<script>
+import Icon from "$lib/com/icon/Icon.svelte"
+</script>
+
+
+<button data-tid="play-album-btn" type="button" class="btn btn-circle btn-ghost text-white/90 hover:text-white" on:click>
+    <Icon icon="mdi:play" size={24}/>
+</button>
