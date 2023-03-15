f555de2043449aefc22fe18e9398a99accb0777c
Craig Hopgood <searaig@gmail.com>
2023-03-15 10:06:00 -0400

    ability to ovveride default meta tags in createMetaTags() fn

--- src/lib/model/meta-tags-factory.js
+++ src/lib/model/meta-tags-factory.js
@@ -3,7 +3,6 @@ import { titlecase, tracksDuration } from "$lib/util/string.js"
 
 
 export const createMetaTags = ( type, model ) => {
-    const defaults = createDefaultMetaTags( site )
 
     let tags = []
 
@@ -14,6 +13,19 @@ export const createMetaTags = ( type, model ) => {
         tags = createShareTrackMetaTags( model )
     }
 
+
+    // collect name and property tags
+    const names = tags.map( t => t?.name ).filter( Boolean )
+    const properties = tags.map( t => t?.property ).filter( Boolean )
+
+    // exclude defaults and use the override from tags
+    const defaults = createDefaultMetaTags( site )
+        .filter( t => {
+            if ( t.name && names.includes( t.name ) ) return false
+            if ( t.property && properties.includes( t.property ) ) return false
+            return true
+        } )
+
     return [ ...defaults, ...tags ]
 }
 
@@ -21,9 +33,9 @@ export const createDefaultMetaTags = site => [
     { name: 'author', content: site.author },
     { name: 'keywords', content: site.keywords },
     { name: 'description', content: site.description },
-    { name: 'twitter:card', content: 'summary'},
-    { name: 'twitter:creator', content: '@multimonos'},
-    { name: 'twitter:site', content: '@multimonos'},
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:creator', content: '@multimonos' },
+    { name: 'twitter:site', content: '@multimonos' },
     { property: 'og:type', content: `website` },
 ]
 
@@ -38,13 +50,13 @@ export const createShareAlbumMetaTags = album => [
     { property: 'og:determiner', content: 'the' },
     { property: 'og:locale', content: 'en_CA' },
     { property: 'og:image', content: album.images?.[0].url },
-    { property: 'og:image:alt', content: `Poster for the ${ album.name } ${ titlecase( album.album_type ) }` },
+    { property: 'og:image:alt', content: `Poster for the ${ album.name } ${ album.album_type }` },
 ]
 
 export const createShareTrackMetaTags = track => [
     {
         name: 'title',
-        content: `${ track.name } by multimonos | ${ track.album.name } ${ titlecase(track.album.album_type) } | Track Share | Play track`
+        content: `${ track.name } by multimonos | ${ track.album.name } ${ titlecase( track.album.album_type ) } | Track Share | Play track`
     },
     { property: 'og:url', content: track.links.share },
     { property: 'og:title', content: `${ track.name }` },
