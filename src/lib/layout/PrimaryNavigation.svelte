7df9f88639046bcf82a0c405875d57594edac2d4
Craig Hopgood <searaig@gmail.com>
2023-03-15 15:45:31 -0400

    deprecate /albums route

--- src/lib/layout/PrimaryNavigation.svelte
+++ src/lib/layout/PrimaryNavigation.svelte
@@ -3,6 +3,5 @@ import { route } from "$lib/config/routes.js"
 </script>
 <ul class="menu menu-compact flex flex-col p-0 px-4">
     <li><a class="flex gap-4" href={route('@home')} on:click>home</a></li>
-    <li><a class="flex gap-4" href={route('@albums')} on:click>albums</a></li>
     <li><a class="flex gap-4" href={route('@debug')} on:click>debug</a></li>
 </ul>
