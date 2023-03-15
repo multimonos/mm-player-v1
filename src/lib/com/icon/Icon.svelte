c81c217ae343ee97bd822b3cd431c575d728b331
Craig Hopgood <searaig@gmail.com>
2023-03-15 16:24:09 -0400

    Icon can accept number or string for size

--- src/lib/com/icon/Icon.svelte
+++ src/lib/com/icon/Icon.svelte
@@ -1,34 +1,36 @@
 <script>
-    import 'iconify-icon'
+import 'iconify-icon'
 
 
-    //props
-    export let icon
-    export let size = 'sm'
+//props
+export let icon
+export let size = 'sm'
 
-    //sizes
-    let sizes = {
-        xs: {
-            width: 12,
-            height: 12,
-        },
-        sm: {
-            width: 18,
-            height: 18,
-        },
-        md: {
-            width: 32,
-            height: 32,
-        },
-        lg: {
-            width: 48,
-            height: 48,
-        },
-        xl: {
+//sizes
+let sizes = {
+    xs: {
+        width: 12,
+        height: 12,
+    },
+    sm: {
+        width: 18,
+        height: 18,
+    },
+    md: {
+        width: 32,
+        height: 32,
+    },
+    lg: {
+        width: 48,
+        height: 48,
+    },
+    xl: {
 
-            width: 56,
-            height: 56,
-        }
+        width: 56,
+        height: 56,
     }
+}
+const width = typeof size === 'number' ? size : sizes[size].width
+const height = typeof size === 'number' ? size : sizes[size].height
 </script>
-<iconify-icon {icon} width={sizes[size].width} height={sizes[size].height}></iconify-icon>
+<iconify-icon {icon} {width} {height}/>
