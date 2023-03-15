3a0440010e09cdcd7922ca7bcf7d287addcaab0b
Craig Hopgood <searaig@gmail.com>
2023-03-15 17:43:58 -0400

    add mobile progress to ui

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,24 +1,27 @@
 <script>
 import { service } from "$lib/state-machine/app-machine.js"
 import { PauseEvent, PlayEvent, SkipBackwardEvent, SkipForwardEvent } from "$lib/state-machine/events.js"
-import { LoadingTag } from "$lib/state-machine/tags.js"
+import { LoadingTag, PlayingTag } from "$lib/state-machine/tags.js"
 import { pause, play, skipBackward, skipForward } from "$lib/actions.js"
 import NowPlaying from "$lib/com/NowPlaying.svelte"
 import QueueButton from "$lib/com/button/QueueButton.svelte"
 import Transport from "$lib/com/transport/Transport.svelte"
 import Contained from "$lib/layout/Contained.svelte"
+import MobileProgressBar from "$lib/com/transport/MobileProgressBar.svelte"
 </script>
 
 
 <footer class="btm-nav z-20 border-t-[1px] border-white/10">
 
+
     <Contained>
-
-        <div class="absolute top-0 left-[-1%] h-1 w-[102%] overflow-x-hidden">
-            <progress class="progress progress-primary" value={$service.context.progress} max={$service.context?.track?.duration || 0}></progress>
-        </div>
-
         <div class="w-full flex flex-row items-center justify-between">
+
+            {#if $service.hasTag( PlayingTag )}
+                <div class="absolute w-full top-0 overflow-x-hidden">
+                    <MobileProgressBar value={$service.context.progress} max={$service.context?.track?.duration || 0}/>
+                </div>
+            {/if}
 
             <div class="flex-none pl-2 md:pl-0">
                 <NowPlaying track={$service.context?.track}/>
