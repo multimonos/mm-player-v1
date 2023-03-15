4bab468cc67c6891d333fd7f57040a1ac1d7a411
Craig Hopgood <searaig@gmail.com>
2023-03-15 17:43:30 -0400

    create very simple progressbar for mobile

---
+++ src/lib/com/transport/MobileProgressBar.svelte
@@ -0,0 +1,11 @@
+<script>
+export let value = 0
+export let max = 0
+
+// percentage with 1 decimal precision
+$:percent = Math.ceil( value / max * 1000 ) / 10
+// $:{if(percent > 97)console.log('per',percent, value)}
+</script>
+<div class="h-[2px] w-full bg-neutral-200">
+    <div class="h-[2px] bg-primary" style="width:{percent}%"></div>
+</div>
