7f8cb2088a9c9e62fb600c6cc129dce9f1c8df2a
Craig Hopgood <searaig@gmail.com>
2023-03-16 08:09:40 -0400

    make navbar easier to manage by moving fully into component

--- src/lib/layout/Navbar.svelte
+++ src/lib/layout/Navbar.svelte
@@ -1,28 +1,24 @@
 <script>
-import { service } from "$lib/state-machine/app-machine.js"
 // import DebugToggle from "$lib/com/button/DebugToggle.svelte"
-import Contained from "$lib/layout/Contained.svelte"
 </script>
-<nav class="navbar ">
-    <Contained>
-        <div class="flex-none">
-            <label for="my-drawer" class="btn drawer-button">
+<div class="sticky top-0 z-30 flex h-16 w-full bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content border-b-[1px] border-white/10">
+    <div class="md:container md:mx-auto w-full">
+        <nav class="navbar">
+            <label for="my-drawer" class="drawer-button btn-square btn-xs btn mr-2 cursor-pointer hover:text-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
             </label>
-        </div>
-        <div class="flex-1">
-            <div class="flex items-center">
-                <a href="/" class="font-normal text-md">multimonos</a>
 
-<!--                <kbd class="kbd text-xs ml-2">{$service.value.player} | {$service.context.audioContext?.state}</kbd>-->
-<!--                <DebugToggle/>-->
-
+            <div class="flex-1">
+                <div class="flex items-center">
+                    <a href="/" class="font-normal text-md hover:text-primary">multimonos</a>
+                </div>
             </div>
-        </div>
-        <div class="flex-none">
-            <!-- more -->
-        </div>
-    </Contained>
-</nav>
+
+            <div>
+                test
+            </div>
+        </nav>
+    </div>
+</div>
