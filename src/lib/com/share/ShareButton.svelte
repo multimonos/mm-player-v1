d674b392903a8628bd8cc495f67390378ae46144
Craig Hopgood <searaig@gmail.com>
2023-03-17 14:17:50 -0400

    refactor share buttonos

--- src/lib/com/share/ShareButton.svelte
+++ src/lib/com/share/ShareButton.svelte
@@ -1,23 +1,12 @@
 <script>
-import Button from "$lib/com/button/Button.svelte"
 import Icon from "$lib/com/icon/Icon.svelte"
 import { share } from "$lib/com/share/sharing.js"
 
 
 // props
 export let shareable
-export let classes = 'text-white'
-export let iconSize = 'sm'
-
-// vars
-const hasContent = $$slots.default
-const shape = hasContent ? 'default' : 'circle'
-
-// derived
-classes += hasContent ? ' flex gap-2 ' : ''
 </script>
 
-<Button { shape } color="ghost" {classes} on:click={()=>share(shareable)}>
-    <Icon icon="mdi:export-variant" size={iconSize}/>
-    <slot/>
-</Button>
+<button type="button" class="btn btn-circle btn-ghost text-white" on:click={()=>share(shareable)}>
+    <Icon icon="mdi:export-variant" size="sm"/>
+</button>
---
+++ src/lib/com/share/ShareButtonTiny.svelte
@@ -0,0 +1,12 @@
+<script>
+import TinyButton from "$lib/com/button/TinyButton.svelte"
+import { share } from "$lib/com/share/sharing.js"
+
+
+// props
+export let shareable
+</script>
+
+<TinyButton on:click={()=>share(shareable)} icon="mdi:export-variant">
+    <slot/>
+</TinyButton>
