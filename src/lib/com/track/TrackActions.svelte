1c5d230c2a50590e8e595ea100a05c34a4aac128
Craig Hopgood <searaig@gmail.com>
2023-03-17 14:20:12 -0400

    add track actions for track in album view

---
+++ src/lib/com/track/TrackActions.svelte
@@ -0,0 +1,26 @@
+<script>
+import { queue } from "$lib/actions.js"
+import { createTrackShare } from "$lib/model/share-factory.js"
+import Button from "$lib/com/button/Button.svelte"
+import TinyButton from "$lib/com/button/TinyButton.svelte"
+import ShareButtonTiny from "$lib/com/share/ShareButtonTiny.svelte"
+
+
+export let track
+
+</script>
+
+
+<div class="dropdown dropdown-left bg-">
+    <label tabindex="0">
+        <Button icon="mdi:dots-vertical" size="sm" shape="circle"/>
+    </label>
+    <ul tabindex="0" class="dropdown-content p-2 shadow bg-black rounded-box w-32 flex flex-col gap-2">
+        <li>
+            <TinyButton icon="mdi:playlist-plus" on:click={queue(track)}>Add to queue</TinyButton>
+        </li>
+        <li>
+            <ShareButtonTiny shareable={createTrackShare(track)}>Share track</ShareButtonTiny>
+        </li>
+    </ul>
+</div>
