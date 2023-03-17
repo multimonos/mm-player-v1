b0bb68cb6d010a3c71b740f60307b15aafd6c715
Craig Hopgood <searaig@gmail.com>
2023-03-17 07:52:37 -0400

    add contextual share modal title for album share

--- src/lib/model/share-factory.js
+++ src/lib/model/share-factory.js
@@ -1,6 +1,7 @@
 import { createShareable } from "$lib/com/share/sharing.js"
 
 export const createAlbumShare = album => createShareable({
+    modalTitle: `Share ${album.name}?`,
     url: album.links.share,
     image: album.images?.[0].url,
     title: `${ album.name } by multimonos`,
