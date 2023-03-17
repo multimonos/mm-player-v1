31c9caaaa3e7ecaa41639c54ab95791b2e56863e
Craig Hopgood <searaig@gmail.com>
2023-03-17 10:24:41 -0400

    refactor share modal titles

--- src/lib/model/share-factory.js
+++ src/lib/model/share-factory.js
@@ -1,14 +1,14 @@
 import { createShareable } from "$lib/com/share/sharing.js"
 
 export const createAlbumShare = album => createShareable({
-    modalTitle: `${album.name}? album`,
+    modalTitle: album.name,
     url: album.links.share,
     image: album.images?.[0].url,
     title: `${ album.name } by multimonos`,
 })
 
 export const createTrackShare = track => createShareable({
-    modalTitle: `${track.name} track`,
+    modalTitle: track.name,
     url: track.links.share,
     image: track.album.images?.[0].url,
     title: `${ track.name } - ${track.album.name}`,
