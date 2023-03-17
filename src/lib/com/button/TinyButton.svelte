ea02ed8837a254dae21f60cbadc8985a6018c67e
Craig Hopgood <searaig@gmail.com>
2023-03-17 14:18:04 -0400

    creaete tiny button

---
+++ src/lib/com/button/TinyButton.svelte
@@ -0,0 +1,13 @@
+<script>
+import Icon from "$lib/com/icon/Icon.svelte"
+
+// props
+export let icon = null
+export let iconSize = 'sm'
+</script>
+<button type="button" class="btn btn-xs btn-ghost flex gap-2 flex-nowrap items-center hover:text-primary" on:click>
+    {#if icon}
+        <Icon {icon} size={iconSize}/>
+    {/if}
+    <span class="whitespace-nowrap normal-case font-normal"><slot/></span>
+</button>
