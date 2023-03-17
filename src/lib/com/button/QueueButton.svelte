83c135bfe016acb9760a3f66da99b295c95fc07c
Craig Hopgood <searaig@gmail.com>
2023-03-17 07:51:47 -0400

    remove unused prop

--- src/lib/com/button/QueueButton.svelte
+++ src/lib/com/button/QueueButton.svelte
@@ -3,9 +3,6 @@ import Button from "$lib/com/button/Button.svelte"
 import { drawerOpen } from "$lib/stores.js"
 import Icon from "$lib/com/icon/Icon.svelte"
 
-// props
-export let q = []
-
 // fns
 const toggle = () => $drawerOpen = ! $drawerOpen
 </script>
