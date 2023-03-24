29164ab7ee9e4c4e6f5a8e9d8f559a90aec42451
Craig Hopgood <searaig@gmail.com>
2023-03-24 11:24:17 -0400

    remove unused code

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,32 +1,6 @@
 import {createMeta} from "$lib/model/meta-factory.js"
 
 
-const getFakeAlbums = async ( fetch, count ) => {
-    const url = `https://dummyapi.io/data/v1/post?limit=${ count }`
-    const res = await fetch( url, {
-        headers: {
-            'app-id': '640b34a46b453b3a2e1e5293'
-        }
-    } )
-
-    const json = await res.json()
-
-    const data = json.data
-        .map( ( o, i ) => ({
-            id:i,
-            images: [{url:o.image}],
-            name: o.text,
-            type: 'album',
-            album_type: 'album',
-            links: {share: 'https://google.ca'},
-            tracks: new Array(5).fill(1)
-        }) )
-
-    console.log(JSON.stringify(data,null,2))
-
-    return data
-}
-
 const getAlbums = async ( fetch ) => {
     // get the albums
     const res = await fetch( '/api/albums' )
@@ -38,7 +12,6 @@ export const load = async ( { fetch } ) => {
     // get the albums
 
     // console.log( { res } )
-    // const albums = await getFakeAlbums( fetch, 48 )
     const albums = getAlbums( fetch )
 
     return {
