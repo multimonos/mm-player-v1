a67681140f54433e3aab329f753ad549da7922c4
Craig Hopgood <searaig@gmail.com>
2023-03-26 17:29:52 -0400

    change spacing between hamburger and wordmark in header

--- src/lib/layout/Navbar.svelte
+++ src/lib/layout/Navbar.svelte
@@ -4,7 +4,7 @@
 <div class="sticky top-0 z-30 flex h-16 w-full bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content border-b-[1px] border-white/10">
     <div class="md:container md:mx-auto w-full">
         <nav class="navbar">
-            <label for="my-drawer" class="drawer-button btn-square btn-xs btn mr-2 cursor-pointer hover:text-primary">
+            <label for="my-drawer" class="drawer-button btn-square btn-xs btn mr-4 cursor-pointer hover:text-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
@@ -16,9 +16,7 @@
                 </div>
             </div>
 
-            <div>
-                test
-            </div>
+            <div> <!-- placeholder --> </div>
         </nav>
     </div>
 </div>
