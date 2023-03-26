6f02b41cbced47c5175e1096da99e225e27bd970
Craig Hopgood <searaig@gmail.com>
2023-03-26 17:20:41 -0400

    conditionally render codeblocks in PortableText.svelte with Prism.js

--- src/lib/layout/PortableText.svelte
+++ src/lib/layout/PortableText.svelte
@@ -1,24 +1,58 @@
 <script>
+import { onMount } from "svelte"
 import { toHTML } from "@portabletext/to-html"
 import { sanityImageUrl } from "$lib/service/sanity-client.js"
 
 // props
 export let text
 
+// vars
+let hasCodeblock = false
+
+// fns
+let renderCodeblock // no need to always load prism.js
+
+const renderImageBlock = ( { value } ) => {
+    return `<img src="${ sanityImageUrl( value ).width( 900 ).auto( 'format' ) }"/>`
+}
+
+const createRenderCodeblockFn = async blocks => {
+    const hasCodeblock = blocks.filter( blk => blk._type === 'codeblock' ).length > 0
+
+    // early exit
+    if ( ! hasCodeblock ) {
+        return ( { value } ) => '<!-- do not load prism.js -->'
+    }
+
+    // load Prism.js
+    const module = await import('prismjs')
+    await import ("prismjs/themes/prism-dark.min.css")
+    const Prism = module.default
+
+    return ( { value } ) => {
+        const code = Prism.highlight( value.code, Prism.languages[value.language] )
+        return `<pre>${ code }</pre>`
+    }
+}
+
+// load
+onMount( async () => {
+    renderCodeblock = await createRenderCodeblockFn( text )
+} )
+
 // reactives
 $:html = toHTML( text, {
     components: {
         types: {
-            image: ( { value } ) => {
-                console.log(value)
-               return `<img src="${ sanityImageUrl(value) }"/>`
-            },
+            image: renderImageBlock,
+            codeblock: renderCodeblock
         }
     }
 } )
+
 </script>
 {#if text}
-    <section class="px-4 xl:pr-2 pb-6 prose">
+    <section class="px-4 pb-6 prose md:max-w-[80ch] xl:pr-2">
         {@html html}
     </section>
 {/if}
