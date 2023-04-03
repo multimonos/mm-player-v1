45cc6399e89c03234276d8698ee4fb64eae714ca
Craig Hopgood <searaig@gmail.com>
2023-04-03 10:56:16 -0400

    better faking

--- src/routes/sketch/[filename]/+page.js
+++ src/routes/sketch/[filename]/+page.js
@@ -1,3 +1,6 @@
+import { fakeTrack } from "./fake-track.js"
+
+
 const getAudioResources = async promise => {
     const res = await promise
     if ( ! res.ok ) return []
@@ -11,18 +14,38 @@ const getSketch = async path => {
 }
 
 export const load = async ( { fetch, url, params } ) => {
-    const audioResources = await getAudioResources( fetch( 'https://mm-media.netlify.app/.netlify/functions/audio' ) )
-    const sketchSlug = params.filename
-    const sketchPath = `/src/routes/sketch/${ params.filename }.js`
+    const sketchpath = `/src/routes/sketch/${ params.filename }.js`
 
+    // custom audioUrl
     const audioUrl = url.searchParams.has( "audioUrl" )
         ? url.searchParams.get( "audioUrl" )
-        : null
+        : "https://res.cloudinary.com/multimonos/video/upload/v1618331073/audio/animals/bbc_giant-toad_nhu0501904.mp3"
+
+    // custom duration - seconds
+    const duration = url.searchParams.has( "du" )
+        ? url.searchParams.get( "du" ) * 1000
+        : false
+
+    // audio resource list
+    const resources = await getAudioResources( fetch( 'https://mm-media.netlify.app/.netlify/functions/audio' ) )
+    const audioResources = resources.map( r => ({ ...r, "selected": r.url == audioUrl }) )
+
+    // fake a track
+    const track = fakeTrack( {
+        slug: params.filename,
+        sketchpath,
+        duration,
+        audioUrl,
+        params: {}
+    } )
+
+    console.log( JSON.stringify( track, null, 2 ) )
 
     return {
+        track,
         audioUrl,
         audioResources,
-        sketchPath,
-        sketchSlug
+        sketchpath,
+        slug: params.filename
     }
 }
--- src/routes/sketch/[filename]/fake-track.js
+++ src/routes/sketch/[filename]/fake-track.js
@@ -1,38 +1,38 @@
 export const fakeTrack = (
     {
-        name = 'sketch',
-        sketchpath = '/src/routes/sketch/sketch.js',
+        slug = 'sketch',
+        sketchpath = `/src/routes/sketch/${ slug }.js`,
+        audioUrl = 'https://res.cloudinary.com/multimonos/video/upload/v1618331761/audio/animals/bbc_western-di_nhu0510004.mp3',
         duration = false,
-        params = {
-            audioUrl: "https://res.cloudinary.com/multimonos/video/upload/v1618331761/audio/animals/bbc_western-di_nhu0510004.mp3",
-        }
+        params = {}
     }
 ) => ({
-    "_key": "7222867ffc3c",
-    "_type": "track",
-    "album": {
-        "_id": "42348b4d-7f49-4888-acd1-562e69ae093e",
-        "_type": "album",
-        "album_type": "single",
-        "links": {
-            "href": "http://localhost:5173/albums/sketching",
-            "self": "http://localhost:5173/api/albums/42348b4d-7f49-4888-acd1-562e69ae093e",
-            "share": "http://localhost:5173/share/multimonos:album:42348b4d-7f49-4888-acd1-562e69ae093e"
+    _key: "7222867ffc3c",
+    _type: "track",
+    name: slug,
+    album: {
+        _id: `${ slug }-fake-id`,
+        _type: "album",
+        album_type: "single",
+        links: {
+            href: `http://localhost:5173/albums/${ slug }`,
+            self: `http://localhost:5173/api/albums/${ slug }-fake-id`,
+            share: `http://localhost:5173/share/multimonos:album:${ slug }-fake-id`
         },
-        "name": "Sketching",
-        "poster": "/1.png",
-        "slug": "sketch"
+        name: "sketching",
+        poster: "/1.png",
+        slug: `${ slug }`
     },
-    duration,
-    "links": {
-        "self": "http://localhost:5173/api/albums/42348b4d-7f49-4888-acd1-562e69ae093e/tracks/7222867ffc3c",
-        "share": "http://localhost:5173/share/multimonos:track:42348b4d-7f49-4888-acd1-562e69ae093e:7222867ffc3c"
+    links: {
+        self: `http://localhost:5173/api/albums/${ slug }-fake-id/tracks/7222867ffc3c`,
+        share: `http://localhost:5173/share/multimonos:track:${ slug }-fake-id:7222867ffc3c`
     },
-    "media": {
-        "_type": "media",
-        "media_type": "p5js",
+    media: {
+        _type: "media",
+        media_type: "p5js",
+        audioUrl,
+        duration,
         params,
-        "url": sketchpath
+        url: sketchpath
     },
-    "name": name,
 })
