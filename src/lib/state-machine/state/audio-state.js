0734433702ee10afefd3acd1f35dcb55ba90c4c3
Craig Hopgood <searaig@gmail.com>
2023-04-10 17:12:35 -0400

    update audio context methods, remove logging

--- src/lib/state-machine/state/audio-state.js
+++ src/lib/state-machine/state/audio-state.js
@@ -2,39 +2,37 @@ import { isAnyAudioContext } from "standardized-audio-context"
 import { IdleState } from "$lib/state-machine/states.js"
 import { AudioResumeEvent,AudioPauseEvent } from "$lib/state-machine/events.js"
 
+// BUG : chrome does not resume/ stop correctly there is a delay...also, present when using audio inspector
+
 export const audioState = {
     initial: IdleState,
     states: {
         [IdleState]: {
             on: {
                 [AudioPauseEvent]: {
-                    actions: ( context ) => {
+                    actions: async ( context ) => {
                         // aggressively create the AudioContext
                         if ( ! isAnyAudioContext( context.audioContext ) ) {
                             context.audioContext = new AudioContext()
-                            console.log( AudioPauseEvent, 'AudioContext created', context.audioContext )
                         }
 
                         if ( isAnyAudioContext( context.audioContext ) && context.audioContext.state === 'running' ) {
-                            context.audioContext.suspend()
-                            console.log( AudioPauseEvent, 'state=', context.audioContext.state )
+                            await context.audioContext.suspend()
                         }
                     }
                 },
                 [AudioResumeEvent]: {
-                    actions: ( context ) => {
+                    actions: async ( context ) => {
                         // NOTE //
                         // It's best to combine the create() + resume() ops into single function, bc, the two calls must occur within ~1sec,
                         // to work for iphone.
                         // We want to call this more times than necessary, so, we always have an available "running" AudioContext.
                         if ( ! isAnyAudioContext( context.audioContext ) ) {
                             context.audioContext = new AudioContext()
-                            console.log( AudioResumeEvent, 'AudioContext created', context.audioContext )
                         }
 
                         if ( isAnyAudioContext( context.audioContext ) && context.audioContext.state === 'suspended' ) {
-                            context.audioContext.resume()
-                            console.log( AudioResumeEvent, 'state=', context.audioContext.state )
+                            await context.audioContext.resume()
                         }
                     }
                 }
