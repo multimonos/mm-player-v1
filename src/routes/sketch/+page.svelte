9f84136af4812c444925128a11e9723612a42798
Craig Hopgood <searaig@gmail.com>
2023-11-09 11:00:04 -0500

    add docs + remove comments

--- src/routes/sketch/+page.svelte
+++ src/routes/sketch/+page.svelte
@@ -6,29 +6,43 @@ export let data
 const sketches = data.sketches || []
 </script>
 <Contained>
-    <section class="mt-4 prose">
-        <h2>How to use</h2>
-        <ul>
-            <li>Access sketches via <code>/sketch/&lt;filename&gt;</code></li>
-            <li>If sketch path has is a subdirectory then replace <code>/ --> %2F</code></li>
-        </ul>
+    <div class="flex mt-4">
+        <section class="flex-1 prose">
+            <h2>Sketches</h2>
+            {#if sketches.length}
+                <ul>
+                    {#each sketches as sketch}
+                        <li><a class="no-underline hover:underline" target="_blank" href="{sketch.url}"><code>{sketch.path}</code></a></li>
+                    {/each}
+                </ul>
+            {:else}
+                <p>None</p>
+            {/if}
+        </section>
 
-        <p>For example,</p>
-        <ul>
-            <li><code>/sketch/sketches/foobar.js</code> then url is <code>/sketch/foobar/</code></li>
-            <li><code>/sketch/sketches/foobar/bam.js</code> then url is <code>/sketch/foobar%2Fbam</code></li>
-        </ul>
-
-        <h2>Sketches</h2>
-        {#if sketches.length}
+        <section class="flex-2 prose prose-a:text-normal">
+            <h2>How to use</h2>
             <ul>
-                {#each sketches as sketch}
-                    <li><a class="no-underline hover:underline" target="_blank" href="{sketch.url}"><code>{sketch.path}</code></a></li>
-
-                {/each}
+                <li>Access sketches via <code>/sketch/&lt;filenpath-no-extension&gt;/</code></li>
+                <li>If filepath is a subdirectory then replace <code>/</code> with <code>%2F</code> in urls</li>
             </ul>
-        {:else}
-            <p>None</p>
-        {/if}
-    </section>
+
+            <p>For example,</p>
+            <ul>
+                <li><code>/sketch/sketches/foobar.js</code> then url is <code>/sketch/foobar/</code></li>
+                <li><code>/sketch/sketches/foobar/bam.js</code> then url is <code>/sketch/foobar%2Fbam/</code></li>
+            </ul>
+
+            <h2>Notes</h2>
+            <ul>
+                <li>run <code>npm run dev</code> in <code>mm-com</code> codebase, aka sveltekit, to refresh sketches &mdash; not out of <code>mm-media</code></li>
+                <li>work out of the <code>mm-media</code> codebase not the <code>mm-com</code> codebase</li>
+                <li><code>mm-com</code> repository is at <a href="https://github.com/multimonos/mm-sandbox" target="_blank">git@github.com:multimonos/mm-sandbox.git</a></li>
+                <li><code>mm-media</code> repository is at <a href="https://github.com/multimonos/mm-media" target="_blank">git@github.com:multimonos/mm-media.git</a></li>
+                <li><code>/src/routes/sketch/sketches/</code> is symlinked to <code>mm-media/src/sketches</code></li>
+                <li><code>/src/routes/sketch/sketches/lib/</code> is symlinked to <code>mm-media/src/lib</code></li>
+            </ul>
+        </section>
+
+    </div>
 </Contained>
--- src/routes/sketch/[filename]/+page.js
+++ src/routes/sketch/[filename]/+page.js
@@ -17,16 +17,19 @@ const getSketch = async path => {
 
 export const load = async ( { fetch, url, params } ) => {
     const sketchpath = `/src/routes/sketch/sketches/${ params.filename }.js`
+    //console.log({sketchpath})
 
     // custom audioUrl
     const audioUrl = url.searchParams.has( "audioUrl" )
         ? url.searchParams.get( "audioUrl" )
         : "https://res.cloudinary.com/multimonos/video/upload/v1618331073/audio/animals/bbc_giant-toad_nhu0501904.mp3"
+    //console.log({audioUrl})
 
     // custom duration - seconds
     const duration = url.searchParams.has( "du" )
         ? url.searchParams.get( "du" ) * 1000
         : DEFAULT_DURATION
+    //console.log({duration})
 
     // fake a track
     const track = fakeTrack( {
@@ -36,9 +39,16 @@ export const load = async ( { fetch, url, params } ) => {
         audioUrl,
         params: {}
     } )
+    // console.log({track})
+
 
     // audio resource list
-    const resources = await getAudioResources( fetch( 'https://mm-media.netlify.app/.netlify/functions/audio' ) )
+    const audioResourcesUrl= 'https://mm-media.netlify.app/audio'
+    // const audioResourcesUrl= 'https://654ab98e13b4522db10413ab--mm-media.netlify.app/manifest'
+    // const audioResourcesUrl= 'http://localhost:8888/audio'
+    const resources = await getAudioResources( fetch( audioResourcesUrl) )
+    // console.log({resources})
+
     const audioResources = resources
         .sort( ( a, b ) => a.title < b.title ? -1 : 1 )
         .map( r => ({ ...r, "selected": r.url == audioUrl }) )
@@ -50,6 +60,7 @@ export const load = async ( { fetch, url, params } ) => {
         slug: params.filename,
         log: url.searchParams.has( 'log' )
     }
+    // console.log({data})
 
     // logging
     if ( url.searchParams.has( 'log' ) ) {
