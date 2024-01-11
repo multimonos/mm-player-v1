a71a53f13c8357bc945a9771d7a24f8795dd0bf9
Craig Hopgood <searaig@gmail.com>
2024-01-11 13:37:17 -0500

    sketches should always look to the mm-sketch/sketches endpoint as it's not possible to set context/env based netlify redirects for mm-api

--- src/routes/sketch/+page.js
+++ src/routes/sketch/+page.js
@@ -1,12 +1,12 @@
 
-import { PUBLIC_API_BASEURI } from "$env/static/public"
+import { PUBLIC_SKETCH_API_BASEURI } from "$env/static/public"
 
 
 
 export const load = async ( { params } ) => {
 
     return {
-        BASEURI_API: PUBLIC_API_BASEURI
+        BASEURI_API: PUBLIC_SKETCH_API_BASEURI
     }
 
 }
