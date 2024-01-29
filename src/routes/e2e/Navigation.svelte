5d3ae0f8e937b1a421762415bc5c22d2b647b6b8
Craig Hopgood <searaig@gmail.com>
2024-01-29 18:25:57 -0500

    simplify test nav

--- src/routes/e2e/Navigation.svelte
+++ src/routes/e2e/Navigation.svelte
@@ -3,23 +3,8 @@
         <h2 class="text-red-700"><a href="/e2e/home" target="_top" >Home</a></h2>
     </li>
 
-    <li>
-        <h2 class="text-red-700 mb-2">SketchPlayer</h2>
-        <ul class="flex flex-col" >
-            <li><a href="/e2e/sketch-audio-single/">audio.single</a></li>
-            <li><a href="/e2e/media-element-variants/">audio.variants</a></li>
-        </ul>
-    </li>
-
-    <li>
-        <h2 class="text-red-700 mb-2">Single</h2>
-        <ul class="flex flex-col" >
-            <li><a href="/e2e/single-data/">single.data</a></li>
-        </ul>
-    </li>
-
+    <li><a href="/e2e/single-data/">single.data</a></li>
     <li><a href="/e2e/list/">list</a></li>
-    <li><a href="/e2e/variants">variants</a></li>
 </ul>
 <style>
     li {
